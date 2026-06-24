88346f94e2da4ca75b79cf9352e2d9b549084798
Vivek Bhushan <vivekbhushan604@gmail.com>
2026-06-24 17:50:17 +0530

    Day 11 assignment add persoon class program.

---
+++ Day_11/Assignment/personClass.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, age, city):
+        self.name = name
+        self.age = age
+        self.city = city
+
+    def profile(self):
+        print("\nPerson Profile")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"City: {self.city}")
+
+
+person1 = Person("Vivek", 22, "Dehradun")
+
+person1.profile()
